49e955d99917e13f17743a3a0f55952b7a8beee3
indhujaprabu <91797260+indhujaprabu@users.noreply.github.com>
2021-10-03 12:18:49 +0530

    Add files via upload

---
+++ PrimeNumber.java
@@ -0,0 +1,14 @@
+package week1day1;
+
+public class PrimeNumber {
+
+	public static void main(String[] args) {
+		int num = 13;
+		System.out.println("FIND PRIME NUMBER");
+		if (num % 2 == 0)
+			System.out.println(num + " is a Prime number");
+		else
+			System.out.println(num + " is not a Prime number");
+	}
+
+}
