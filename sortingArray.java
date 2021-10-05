02090192ca0f97baf54854b11fca9f908e021ac4
indhujaprabu <91797260+indhujaprabu@users.noreply.github.com>
2021-10-05 15:22:58 +0530

    Add files via upload

---
+++ sortingArray.java
@@ -0,0 +1,20 @@
+package week1.assignments;
+import java.util.Arrays;
+
+public class MIssingElementArray {
+
+	public static void main(String[] args) {
+	
+		int[] arr= {1,2,3,4,7,6,8};
+		System.out.println("Sort the Array");
+		Arrays.sort(arr);
+		for( int i=0;i<arr.length;i++)
+		{
+			System.out.println(arr[i]);
+	
+		}
+		
+
+	}
+
+}
