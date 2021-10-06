b1fb667b9a5c2393e736bcc96e3d10f89c7e5892
indhujaprabu <91797260+indhujaprabu@users.noreply.github.com>
2021-10-06 14:19:02 +0530

    Add files via upload

---
+++ Palindrome.java
@@ -0,0 +1,18 @@
+package week1.assignments;
+
+public class Palindrome {
+
+	public static void main(String[] args) {
+
+		String str = "madam";
+		String str1 = "madam";
+		System.out.println("The given string is " + str);
+
+		if (str.equals(str1))
+			System.out.println("Palindrome");
+		else
+			System.out.println("not a Palindrome");
+
+	}
+
+}
